bbbc860d65315b4afa8a13c04f6b47de323fe159
clOUdy <riyanshverma@jklu.edu.in>
2025-02-18 16:50:26 +0530

    Nothing much

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import express from "express"
 import bodyParser from "body-parser"
 import axios from "axios"
 
+
 const app = express()
 const port = 3000
+const Weather_API = "openuv-d24zcrm7a5saos-io"
+const LogIn_API = "http://localhost:4000"
 
-app.get("/",(req, res)=>{
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.get("/", (req, res)=>{
     res.status(200).send("<h1>Hello world</h1>");
 });
 
 app.listen(port, (req, res)=>{
-    console.log(`Listening to port ${port}`)
+    console.log(`Backend server is running on http://localhost:${port}`)
 })
--- server.js
+++ server.js
@@ -9,11 +9,15 @@ const credentials = {
     password: "password",
 }
 
-app.get("/", (req, res)=>{
+app.get("/confirmation", (req, res)=>{
     if(req.body.username === credentials.username && req.body.password === credentials.password){
         res.status(200).send("Login successful")
     }
     else{
         res.status(401).send("Invalid username or password")
     }
+})
+
+app.listen(port, ()=>{
+    console.log(`API Server running at http://localhost:${port}`)
 })
